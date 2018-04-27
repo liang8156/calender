0e93331edb80420c4281f2e3005ae5d10290949d
alexLiang <liang8156@gmail.com>
2018-04-27 23:45:44 +0800

    差美化 基本上都好了

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
+function select_value() {
+    year = document.getElementById("selectYear").value
+    month = document.getElementById("selectMonth").value
+    calendar(year, month);
+}
+
 function drawCal(inc) {
     month += inc;
     if (month == 0) {
@@ -12,43 +18,62 @@ function drawCal(inc) {
 }
 
 function calendar(yr, mth) {
-    var td = new Date();
-    var today = new Date(td.getFullYear(), td.getMonth(), td.getDate());
-    var dt = new Date(yr, mth - 1, 1);
-    var wd = dt.getDay(); //week day
 
-    s = '<table>';
-    s += '<tr>';
-    s += '<th onclick="drawCal(-1)"><<<th colspan=5>'
-        + yr + '年' + mth + '月<th onclick="drawCal(1)">>>';
-    s += '</tr>';
-    s += '<tr>';
-    s += '<th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th>'
-    s += '</tr>';
+    var dt = new Date(yr, mth - 1, 1);
+    var wd = dt.getDay();               //判斷是星期幾 於39行用來畫月曆
+
+
+    cal = '<table>';
+    cal += '<tr class=calendarTitle>';
+    cal += '<th  colspan=5>' + yr + '年' + mth + '月' +
+        '<th onclick="drawCal(-1)">∧<th onclick="drawCal(1)">∨';
+    cal += '</tr>';
+    cal += '<tr>';
+    cal += '<th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th>'
+    cal += '</tr>';
 
     mth--;
-    s += '<tr>';
+    cal += '<tr>';
+    for (; wd > 0; wd--) {
+        cal += '<td/>'; //用來補月數剛開始的空格
+    }
     for (i = 1; i < 32;) {
         dt = new Date(yr, mth, i);
         wd = dt.getDay();
-
-        if (wd == 0) {
-            s += '<tr>';
+        if (dt.getMonth() != mth) {
+            //判斷月數 若不符合跳出(例如4/31會變成5/1 此時4!=5 就會跳出) 
+            break;
         }
-        s += '<td>' + i + '</td>';
+        if (wd == 0) {
+            cal += '<tr>';
+        }
+        cal += '<td>' + i + '</td>';
         i++;
         if (wd == 6) {
-            s += '</tr>';
+            cal += '</tr>';        //wd==0 和wd==6之間由<tr></tr>刮起來 由此循環
         }
     }
-    s += '</tr>';
-    s += '</table>';
-    document.getElementById('calendar').innerHTML = s;
+    cal += '</tr>';
+    cal += '</table>';
+    document.getElementById('calendar').innerHTML = cal;
 }
 
+a = '<select  id="selectYear" onchange="select_value();">'
+for (i = 2000; i < 2030; i++) {
+    a += '<option value="' + i + '">' + i + '</option>';
+}
+a += '</select>'
 
-var td = new Date();
+a += '<select  id="selectMonth" onchange="select_value();">'
+for (i = 1; i <= 12; i++) {
+    a += '<option value="' + i + '">' + i + '</option>';
+}
+a += '</select>'
+
+document.getElementById('selectYM').innerHTML = a;
+var td = new Date();           //初始值,剛加入未調的得到的初始日期
 var year = td.getFullYear();
-var month = td.getMonth() + 1;
+var month = td.getMonth() + 1; //因為1月時是從0開始算 故給值時月數+1
 calendar(year, month);
+
 
